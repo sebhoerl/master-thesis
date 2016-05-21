43d3ff20dd3196b3b8f1d25036364fbd81bc9b9b
Sebastian Hörl <hoerl.sebastian@gmail.com>
2016-05-21 08:45:34 +0800

    final draft

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ cp ../references.bib .
 cp -r ../chapters .
 cp ../*.tex .
 cp ../cover.pdf .
+cp ../title.pdf .
+cp ../imprint.pdf .
+cp ../abstract.pdf .
 pdflatex thesis.tex
 bibtex thesis
 pdflatex thesis.tex
